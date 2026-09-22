d5ca3f9f7de9602e55d7186d49d5c5e881c604ed
Aidan Timson <aidan@timmo.dev>
2026-09-22 10:22:49 +0100

    Enable type-aware linting and update dependencies

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -3,4 +3,7 @@ import recommendedEffect from "@timmo001/oxlint-rules/configs/recommended-effect
 
 export default defineConfig({
   extends: [recommendedEffect],
+  options: {
+    typeAware: true,
+  },
 });
